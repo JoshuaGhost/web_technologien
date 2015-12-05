9ada7d4d8dc566a274d8c8c877e49a1c67bd79b3
Zhang Zijian <zhangzijian0523@gmail.com>
2015-12-05 23:26:43 +0100

    find out a way to build slide button, source code still need to be arraged.

---
+++ web/scripts/home.js
@@ -0,0 +1,6 @@
+document.write("hallo from home.js");
+$(document).ready(function(){
+    $("img.imc").onmouseover(function(){
+        this.hide();
+    });
+});
---
+++ web/scripts/index.js
@@ -0,0 +1,37 @@
+
+function detailSlideIn(self) {
+    ul = $(self).children("ul");
+    li=ul.children("li");
+    ul.stop(true).animate({top:-li.height()},600);
+}
+
+function detailSlideOut(self) {
+    ul = $(self).children("ul");
+    li = ul.children("li");
+    ul.stop(true).animate({top:0}, 600);
+
+}
+    /*
+    $(document).ready(function(){
+    $("a.ad").hide();
+});
+    var detailId=self.id+"d"
+    console.log(detailId);
+    var detail=document.getElementById(detailId);
+    console.log(detail);
+    $(self).animate({height:"100px", width:"0px"});
+    //$(self).hide();
+    $(detail).animate({height:"100px", width:"100px"});
+    //$(detail).show();
+}
+function contentSlideIn(self) {
+    var contentId=self.id.substring(0,2)
+    console.log(contentId);
+    var content=document.getElementById(contentId);
+    console.log(content);
+    $(self).animate({height:"100px", width:"0px"});
+    //$(self).hide();
+    $(content).animate({height:"100px", width:"100px"});
+    //$(content).show();
+}
+*/
